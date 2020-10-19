11a19e9deb87054764dfe9bd613d66c483fffd26
vtspp <vtsppinho@gmail.com>
2020-10-18 22:55:47 -0300

    Start Project

---
+++ src/main/java/com/github/vtspp/MongoDbNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.vtspp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbNoSqlApplication.class, args);
+	}
+
+}
