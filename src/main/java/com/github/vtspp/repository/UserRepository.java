920cd9327fc02511bf51f409df987a933971f013
Your Name <you@example.com>
2020-10-21 12:47:51 -0300

    @NoRepositoryBean annotation and refinement of the middle class

--- src/main/java/com/github/vtspp/repository/UserRepository.java
+++ src/main/java/com/github/vtspp/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.github.vtspp.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.github.vtspp.domain.User;
 
-@Repository
-public interface UserRepository extends Repositories<User, String>{
+public interface UserRepository extends Repositories<User, String> {
 
 }
